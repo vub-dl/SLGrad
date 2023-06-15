23a8dc653e16d74d5d87299b9d917d5d407cf98d
Emilie Grégoire <108074099+EmiGregoire@users.noreply.github.com>
2023-06-15 17:12:01 +0200

    Added backbone related code

---
+++ Backbones/RandomScaleCrop.py
@@ -0,0 +1,21 @@
+import random
+import torch.nn.functional as F
+
+class RandomScaleCrop(object):
+    """
+    Credit to Jialong Wu from https://github.com/lorenmt/mtan/issues/34.
+    """
+    def __init__(self, scale=[1.0, 1.2, 1.5]):
+        self.scale = scale
+
+    def __call__(self, img, label, depth, normal):
+        height, width = img.shape[-2:]
+        sc = self.scale[random.randint(0, len(self.scale) - 1)]
+        h, w = int(height / sc), int(width / sc)
+        i = random.randint(0, height - h)
+        j = random.randint(0, width - w)
+        img_ = F.interpolate(img[None, :, i:i + h, j:j + w], size=(height, width), mode='bilinear', align_corners=True).squeeze(0)
+        label_ = F.interpolate(label[None, None, i:i + h, j:j + w], size=(height, width), mode='nearest').squeeze(0).squeeze(0)
+        depth_ = F.interpolate(depth[None, :, i:i + h, j:j + w], size=(height, width), mode='nearest').squeeze(0)
+        normal_ = F.interpolate(normal[None, :, i:i + h, j:j + w], size=(height, width), mode='bilinear', align_corners=True).squeeze(0)
+        return img_, label_, depth_ / sc, normal_
